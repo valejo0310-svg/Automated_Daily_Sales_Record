16a1414cb35f66a7d40cf1598d15345689b53643
Valery <Valejo0310@gmail.com>
2026-03-10 15:51:14 -0500

    First Draft for Automated Daily Sales along whit the functions

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print ("Welcome to the Sales Record for Riwi Store!!")
+from time import sleep
+from Calculator import registrar_ventas
+from Show_message import mostrar_resumen
+
+
+
+
+def main():
+
+    welcome = """
+╔════════════════════════════════════════════════════════════╗
+        WELCOME TO THE SALES RECORD RIWI STORE!!
+╚════════════════════════════════════════════════════════════╝
+"""
+
+    for i in welcome:
+        print(i, end="", flush=True)
+        sleep(0.0005)
+
+
+    continuar = "s"
+
+    while continuar.lower() == "s":
+        registrar_ventas()
+        continuar = input("¿Desea registrar otra venta? (s/n): ")
+
+    mostrar_resumen()
+
+
+main()
